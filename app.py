4fff374783773e33d1b113a28a95b7ae1ba20834
Muhammad Nasir <falconpak42@gmail.com>
2025-04-13 16:14:32 +0500

    Web app is live

--- app.py
+++ app.py
@@ -20,5 +20,13 @@ def predict_api():
   output = regmodel.predict(new_data)
   return jsonify(output[0])
 
+@pp.route('/predict', methods = ['POST'])
+def predict():
+  data = [float(x) for x in request.form.values()]
+  final_input = scalar.transform(np.array(data).reshape(1,-1))
+  print(final_input)
+  output = regmodel.predict(final_input)[0]
+  return render_template("home.html", predictin_text = "The house price prediction is {}".format(output))
+
 if __name__ == "__main__":
   app.run(debug=True)
