dc81efee5fd9db4db22e421f134cf3b832e3988c
Muhammad Nasir <falconpak42@gmail.com>
2025-04-17 06:41:00 +0500

    A few modification in the web page.

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import pickle
 from flask import Flask,request,app, jsonify, url_for, render_template
 import numpy as np
 import pandas as pd
+from sklearn.utils.validation import check_array
 
 app = Flask(__name__ )
 regmodel = pickle.load(open('regmodel.pkl', 'rb'))
+scaler = pickle.load(open('scaling.pkl', 'rb')) 
 
 @app.route('/')
 def home():
@@ -16,14 +18,14 @@ def predict_api():
   data = request.json['data']
   print(data)
   print(np.array(list(data.values())).reshape(1,-1))
-  new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
+  new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
   output = regmodel.predict(new_data)
   return jsonify(output[0])
 
 @app.route('/predict', methods = ['POST'])
 def predict():
   data = [float(x) for x in request.form.values()]
-  final_input = scalar.transform(np.array(data).reshape(1,-1))
+  final_input = scaler.transform(np.array(data).reshape(1,-1))
   print(final_input)
   output = regmodel.predict(final_input)[0]
   return render_template("home.html", predictin_text = "The house price prediction is {}".format(output))
